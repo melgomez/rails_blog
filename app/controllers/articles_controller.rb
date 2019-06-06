28a9445324a6a6086d42f877270d46e1fd622bca
Mel Gomez <meldarrelgomez@gmail.com>
2019-06-06 16:30:00 +0800

    Add Edit Article function and view files

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,13 +6,35 @@ class ArticlesController < ApplicationController
   def create
     # render plain: params[:article].inspect
     @article = Article.new(article_params);
-    @article.save;
-    redirect_to articles_show(@article);
+    if @article.save
+      flash[:notice] = "Article created successfully!";
+      redirect_to article_path(@article);
+    else
+      render :new;
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id]);
+  end
+
+  def edit
+    @article= Article.find(params[:id]);
+    render :new;
+  end
+
+  def update
+    @article= Article.find(params[:id]);
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated!"
+      redirect_to article_path(@article);
+    else
+      redirect_to article_edit_path
+    end
   end
 
   private
   def article_params
     params.require(:article).permit(:title, :description);
   end
-
 end
