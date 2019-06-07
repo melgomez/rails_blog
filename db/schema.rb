4e3da4b8066124a5e4920c51dfb47326c6b565bc
Mel Gomez <meldarrelgomez@gmail.com>
2019-06-07 18:02:43 +0800

    Add user to article association.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_07_073313) do
+ActiveRecord::Schema.define(version: 2019_06_07_082051) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
