741042e44b014db8a10077ac25ae39912671bc89
Mel Gomez <meldarrelgomez@gmail.com>
2019-06-08 00:25:46 +0800

    Integrate bcrypt for authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
   has_many :comments
   has_many :articles
-  
+
   before_save { self.email = email.downcase }
 
   validates :username, presence: true, uniqueness: true,  length: { minimum: 3, maximum: 25};
 
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 105}, uniqueness: { case_sensitive: VALID_EMAIL_REGEX }
+
+  has_secure_password
 end
