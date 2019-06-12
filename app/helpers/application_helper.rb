6197dd2c35734f9e88a192c7ea5cb18889b7b240
melgomez <meldarrelgomez@gmail.com>
2019-06-12 08:16:17 +0800

    Add helper methods to display nav links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,6 +6,13 @@ module ApplicationHelper
     end
   end
 
+  def nav_link_method(link_text, link_path, method)
+    class_name = current_page?(link_path) ? 'nav-item active' : 'nav-item'
+    content_tag(:li, :class => class_name) do
+      link_to link_text, link_path, class: 'nav-link', method: "delete"
+    end
+  end
+
   def gravatar_for(user, options = {size: 80})
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "sampleuser", email: "sampleuser@email.com", password: "samplepassword" )
+
+Article.create(title: "Title Goes Here", description: "Description Goes Here", user: User.last )
+
+Article.create(title: "Title Goes Here", description: "Description Goes Here", user: User.last )
+
+Article.create(title: "Title Goes Here", description: "Description Goes Here", user: User.last )
